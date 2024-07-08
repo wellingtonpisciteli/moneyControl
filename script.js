798ff3567aff7076527905e3d4d3317d1c6338a5
Borges <115740592+wellingtonpisciteli@users.noreply.github.com>
2024-07-08 20:59:00 -0300

    atualizações 08/07

--- script.js
+++ script.js
@@ -5,6 +5,15 @@ const header=document.getElementById("header")
 const telaAdd=document.getElementById("telaAdd")
 const btnConfirma=document.getElementById("btnConfirma")
 const btnVoltar=document.getElementById("btnVoltar")
+const btnVolta=document.getElementById("btnVolta")
+const btnSelecionar=document.getElementById("btnSelecionar")
+const dadosSalvos=document.getElementById("dadosSalvos")
+
+let visuDados=document.getElementById("visuDados")
+let selectData=document.getElementById("selectData")
+let data=document.getElementById("data")
+let nomeDespesa=document.getElementById("nomeDespesa")
+let valorDespesa=document.getElementById("valorDespesa")
 
 btnAdd.addEventListener("click", ()=>{
     header.style.display="none"
@@ -20,7 +29,7 @@ btnAdd.addEventListener("click", ()=>{
         btnConfirma.style.display="flex"
         btnVoltar.style.display="flex"
         container.appendChild(telaAdd)
-    },3000)  
+    },1000)  
 })
 
 btnVoltar.addEventListener("click", ()=>{
@@ -36,5 +45,37 @@ btnVoltar.addEventListener("click", ()=>{
         header.style.display="flex"
         btnAdd.style.display="flex"
         btnVisu.style.display="flex"
-    },3000)
+    },1000)
+})
+
+btnVisu.addEventListener("click",()=>{
+    header.style.display="none"
+    btnAdd.style.display="none"
+    btnVisu.style.display="none"
+    let carregando3=document.createElement("div")
+    carregando3.classList.add("carregando")
+    carregando3.textContent="Carregando..."
+    container.appendChild(carregando3)
+    setTimeout(function(){
+        carregando3.style.display="none"
+        telaVisu.style.display="flex"
+        btnSelecionar.style.display="flex"
+        btnVolta.style.display="flex"
+    },1000)
+})
+
+btnVolta.addEventListener("click", ()=>{
+    btnSelecionar.style.display="none"
+    btnVolta.style.display="none"
+    telaVisu.style.display="none"
+    let carregando4=document.createElement("div")
+    carregando4.classList.add("carregando")
+    carregando4.textContent="Carregando..."
+    container.appendChild(carregando4)
+    setTimeout(function(){
+        carregando4.style.display="none"
+        header.style.display="flex"
+        btnAdd.style.display="flex"
+        btnVisu.style.display="flex"
+    },1000)
 })
